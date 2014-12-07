9eadca97e5e4d60a50e57b93e631f443b95707c8
Szymon Pałucha <paluchasz@googlemail.com>
2014-12-07 15:35:12 +0000

    Intial commit

---
+++ air_mini.py
@@ -0,0 +1,12 @@
+USER = "Szymon Palucha"
+USER_ID = "ggjv86"
+
+def trajectory (launch_speed, launch_angle_deg, num_samples):
+  import numpy
+  x = numpy.array(num_samples, dtype=float)
+  y = numpy.array(num_samples, dtype=float)
+  t = numpy.array(num_samples, dtype=float)
+
+x = launch_speed*cos(launch_angle_deg)*t
+y = launch_speed*sin(launch_angle_deg)*t-0.5*9.81*t^2
+
