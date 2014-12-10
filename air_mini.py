e5d9127ba96fdd619dceafa40cd19199ec31ce82
Szymon Pałucha <paluchasz@googlemail.com>
2014-12-10 14:43:11 +0000

    Added test body to main Tested and corrected trajectory alghorithm Trajectory will return tuple with 3 numpy arrays
    Issue: trajectory is calculated to one step before returning to earth ? This should be corrected

--- air_mini.py
+++ air_mini.py
@@ -6,22 +6,22 @@ def trajectory (launch_speed, launch_angle_deg, num_samples):
   #launch_speed is the initial speed of the air sampling projectile
   #launch_angle_deg is the angle in degrees from the horizontal at which the air sampling projectile is launched
   #num_samples is 
-  launch_speed=raw_input('enter launch_speed')
   
   import numpy
-  x = numpy.array(num_samples, dtype=float)
-  y = numpy.array(num_samples, dtype=float)
-  t = numpy.array(num_samples, dtype=float)
+  x = numpy.arange(num_samples, dtype=float)
+  y = numpy.arange(num_samples, dtype=float)
+  time = numpy.arange(num_samples, dtype=float)
+  
+  t_earth = 2*launch_speed*numpy.sin(launch_angle_deg)/9.81
+  dt = t_earth/num_samples
 
-x = launch_speed*numpy.cos(launch_angle_deg)*t
-y = launch_speed*numpy.sin(launch_angle_deg)*t-0.5*9.81*t^2
-t = (2*y/g)**0.5
+  for t in range(0,num_samples):
+    time[t] =dt*t
+    x[t] = launch_speed*numpy.cos(launch_angle_deg)*time[t]
+    y[t] = launch_speed*numpy.sin(launch_angle_deg)*time[t]-0.5*9.81*time[t]**2
 
-print x
-print y
-print t
+  return(x,y,time)
 
-if__name__="__main__"
 def simplePlot(xVals,yVals):
   import matplotlib.pyplot as pyplot
   pyplot.figure()
@@ -30,3 +30,11 @@ def simplePlot(xVals,yVals):
   pyplot.ylabel('y (m)')
   pyplot.show()
   
+if __name__=='__main__':
+  print 'test plot'
+  # launch_speed=raw_input('enter launch_speed')
+  tuples = trajectory(50,45,50)
+  print tuples[0] #x
+  print tuples[1] #y
+  #print tuples[2] #time
+  simplePlot(tuples[0],tuples[1])
