a6f01ec6d64b584b56bc5c564b851375f96d5774
Szymon Pałucha <paluchasz@googlemail.com>
2014-12-11 15:40:29 +0000

    poprawki 11/12/14

--- air_mini.py
+++ air_mini.py
@@ -1,20 +1,26 @@
+'''This Module returns three one dimensional arrays for vertical and horizontal positions of the projectile at equal time intervals'''
 USER = "Szymon Palucha"
 USER_ID = "ggjv86"
 
-"put doc string here"
 def trajectory (launch_speed, launch_angle_deg, num_samples):
-  """ tutaj napisze documentcje funkcji trajectory, jako zadanie domowe """
+  ''' This method creates three arrays for x y and t. It then uses suvat equations to add values to these arrays''' 
   #launch_speed is the initial speed of the air sampling projectile
   #launch_angle_deg is the angle in degrees from the horizontal at which the air sampling projectile is launched
-  #num_samples is 
+  #num_samples is the number of time samples distributed evenly over the time of flight 
   
   import numpy
+  import math
+  
+  launch_angle_deg = launch_angle_deg*math.pi/180 
+  #This is to convert the angle to radians so the sine/cosine functions can be applied
   x = numpy.arange(num_samples, dtype=float)
   y = numpy.arange(num_samples, dtype=float)
   time = numpy.arange(num_samples, dtype=float)
   
   t_earth = 2*launch_speed*numpy.sin(launch_angle_deg)/9.81
+  #This equation gives the total time of flight
   dt = t_earth/(num_samples-1)
+  #dt is the time interval
 
   for t in range(0,num_samples):
     time[t] =dt*t
@@ -33,9 +39,8 @@ def simplePlot(xVals,yVals):
   
 if __name__=='__main__':
   print 'test plot'
-  # launch_speed=raw_input('enter launch_speed')
-  tuples = trajectory(50,45,50)
+  tuples = trajectory(9.81,45,11)
   print tuples[0] #x
   print tuples[1] #y
-  #print tuples[2] #time
+  print tuples[2] #time
   simplePlot(tuples[0],tuples[1])
