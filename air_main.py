6088bf03f4fc0dd4baa1b21e412f5a73b4703e06
Szymon Pałucha <paluchasz@googlemail.com>
2015-02-07 17:14:59 +0000

    Zmienilem spacje i dodalem komende print

--- air_main.py
+++ air_main.py
@@ -1,7 +1,7 @@
 USER = "Szymon Palucha"
 USER_ID = "ggjv86"
 
-def trajectory_drag(launch_speed,launch_angle_deg, num_samples, m, g, k):
+def trajectory_drag(launch_speed,launch_angle_deg, num_samples, m, g=-9.81, k=0.043):
 	#launch_speed is the initial speed of the air sampling projectile
 	#launch_angle_deg is the angle in degrees from the horizontal at which the air sampling projectile is launched
 	#num_samples is the number of time samples distributed evenly over the time of flight 
@@ -11,25 +11,29 @@ def trajectory_drag(launch_speed,launch_angle_deg, num_samples, m, g, k):
 	import numpy
 	import math
 	
-	g=-9.81, k=0.043
-	
 	launch_angle_deg = launch_angle_deg*math.pi/180
 	#This is to convert the angle to radians so the sine/cosine functions can be applied
 	x = numpy.arange(num_samples, dtype=float)
-    y = numpy.arange(num_samples, dtype=float)
+	y = numpy.arange(num_samples, dtype=float)
 	time = numpy.arange(num_samples, dtype=float)
    
 
- t_earth = -2*launch_speed*numpy.sin(launch_angle_deg)/g
+	t_earth = -2*launch_speed*numpy.sin(launch_angle_deg)/g
 	#This equation gives the total time of flight
 	dt = t_earth/(num_samples-1)
 	#dt is the time interval
 	
+	initial_horiz_speed = launch_speed*numpy.cos(launch_angle_deg)
+	initial_vert_speed = launch_speed*numpy.sin(launch_angle_deg)
 	
-  for t in range(0,num_samples):
-    time[t] =dt*t
-	x[t]=m*launch_speed*numpy.cos(launch_angle_deg)*(1-numpy.exp(-k*time[t]/m))/k
-	y[t]=1/k*(m*g*time[t]+m*(launch_speed*numpy.sin(launch_angle_deg)-m*g/k)*(1-numpy.exp(-k*time[t]/m)))
+	for t in range(0,num_samples):
+		time[t] =dt*t
+		x[t]=m*initial_horiz_speed*(1-numpy.exp(-k*time[t]/m))/k
+		y[t]=1/k*(m*g*time[t]+m*(initial_vert_speed-m*g/k)*(1-numpy.exp(-k*time[t]/m)))
 	
-        	
-
+	return(x,y,time)
+	
+	
+	
+	
+print trajectory_drag(20,45,10,2)
